0fe313436f03b4d2821dacedfba986881ea53796
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-06-28 11:50:14 +0700

    add: route and function delete comment

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -32,8 +32,30 @@ const handleGetCommentByMovieID = async(req,res)=>{
     })
 }
 
+const handleCountComment = async(req,res)=>{
+    let count = await commentService.countComment()
+    return res.status(200).json({
+        errCode: 0,
+        count
+    })
+}
+
+const hanldeDeleteComment = async (req,res)=>{
+    let id = req.body.id
+    if(!id){
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters'
+        })
+    }
+    let message = await commentService.deleteComment(id)
+    return res.status(200).json(message)
+}
+
 module.exports = {
     handleGetAllComments: handleGetAllComments,
     handleCreateNewComment: handleCreateNewComment,
-    handleGetCommentByMovieID: handleGetCommentByMovieID
+    handleGetCommentByMovieID: handleGetCommentByMovieID,
+    handleCountComment: handleCountComment,
+    hanldeDeleteComment: hanldeDeleteComment
 };
